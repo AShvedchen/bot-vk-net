7beb24f4faaed01db9ec1dcdf42e59c1d96c6f8e
admin_redmi <ntnshvd@gmail.com>
2023-03-08 17:02:50 +0300

    start

--- start_vkinder_bot.py
+++ start_vkinder_bot.py
@@ -1,29 +1,39 @@
 from main import *
+import threading
 
-print('Бот готов')
-delete_all_tables(engine)
-create_tables()
-gen = None
-for event in Bot().longpoll.listen():
+gen = {}
+
+def handle_user_event(event):
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         if event.text.lower() == 'поиск':
             Bot().delete_users_find(event.user_id)
             Bot().insert_user(event.user_id)
             Bot().find_users(event.user_id)
-            Bot().write_msg(event.user_id, f'Привет {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
-            gen = select_find_users(event.user_id)
-            Bot().next_user(event.user_id, next(gen), '')
+            Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
+            gen[event.user_id] = select_find_users(event.user_id)
+            Bot().next_user(event.user_id, next(gen[event.user_id]), '')
             Bot().write_msg(event.user_id, f'Жми "Ещё"')
         elif event.text.lower() == 'поиск по':
             Bot().delete_users_find(event.user_id)
             Bot().insert_user(event.user_id)
             Bot().find_users_param(event.user_id)
-            Bot().write_msg(event.user_id, f'Привет {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
-            gen = select_find_users(event.user_id)
-            Bot().next_user(event.user_id, next(gen), '')
+            Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
+            gen[event.user_id] = select_find_users(event.user_id)
+            Bot().next_user(event.user_id, next(gen[event.user_id]), '')
             Bot().write_msg(event.user_id, f'Жми "Ещё"')
-        elif event.text.lower() == 'ещё' and gen is not None:
-            Bot().next_user(event.user_id, next(gen), '')
-            Bot().write_msg(event.user_id, f'Жми "Ещё"')
-        else:
-            pass
+        elif event.text.lower() == 'ещё' and event.user_id in gen:
+            next_user = next(gen[event.user_id])
+            Bot().next_user(event.user_id, next_user, '')
+            Bot().write_msg(event.user_id, f'Нашёл для тебя пару:')
+        elif event.text.lower() == 'ещё' and event.user_id not in gen:
+            Bot().write_msg(event.user_id, 'Сначала начните поиск с помощью команды "Поиск" или "Поиск по"')
+
+def listen_for_events():
+    for event in Bot().longpoll.listen():
+        threading.Thread(target=handle_user_event, args=(event,)).start()
+
+if __name__ == '__main__':
+    print('Бот готов')
+    delete_all_tables(engine)
+    create_tables()
+    listen_for_events()
