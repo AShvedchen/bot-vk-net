828f09aa80c5cfaaeba14fffff70935be66e9753
admin_redmi <ntnshvd@gmail.com>
2023-03-11 17:57:21 +0300

    start

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 from datetime import datetime, date
+
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from random import randrange
 from database_bot import *
 from data_config import *
 from vkinder_keyboard import keyboard
-
 
 class Bot:
     def __init__(self):
@@ -20,14 +20,14 @@ class Bot:
                                          'random_id': randrange(10 ** 7)})
 
     def age_from(self, user_id):
-        self.write_msg(user_id, 'Введите возраст поиска от: ')
-        for event in self.longpoll.listen():
-            if event.type == VkEventType.MESSAGE_NEW and event.to_me:
-                age_from = event.text
-                if age_from.isdigit() and 18 <= int(age_from) <= 70:
-                    return int(age_from)
-                else:
-                    self.write_msg(user_id, 'Возраст должен быть целым числом, попробуйте еще раз.')
+            self.write_msg(user_id, 'Введите возраст поиска от: ')
+            for event in self.longpoll.listen():
+                if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+                    age_from = event.text
+                    if age_from.isdigit() and 18 <= int(age_from) <= 70:
+                        return int(age_from)
+                    else:
+                        self.write_msg(user_id, 'Возраст должен быть целым числом, попробуйте еще раз.')
 
     def age_to(self, user_id):
         self.write_msg(user_id, 'Введите возраст поиска до: ')
@@ -38,6 +38,7 @@ class Bot:
                     return int(age_to)
                 else:
                     self.write_msg(user_id, 'Возраст должен быть целым числом, попробуйте еще раз.')
+
 
     def insert_user(self, user_id):
         response = self.vk.method('users.get', {'user_ids': user_id,
@@ -52,12 +53,8 @@ class Bot:
             user_sex = data['sex']
             user_city = data.get('city', {}).get('id')
             user_city = None if user_city is None else user_city
+
             return insert_users_vk(first_name, last_name, vk_id, user_birthday, user_sex, user_city)
-
-    def delete_users_find(self, user_id):
-        vk_id = user_id
-        return delete_tables(vk_id)
-
     def find_city(self, user_id):
         while True:
             self.write_msg(user_id, 'Введите город для поиска: ')
@@ -73,79 +70,67 @@ class Bot:
                     else:
                         self.write_msg(user_id, 'Город не найден, попробуйте еще раз.')
 
+
     def calculate_age(self, birthdate):
         birthdate = datetime.strptime(birthdate, '%d.%m.%Y')
         today = date.today()
         age = today.year - birthdate.year - ((today.month, today.day) < (birthdate.month, birthdate.day))
         return age
 
-    def find_users(self, user_id):
-        # Получаем значение параметра sex из базы данных
-        sex = select_param_user(user_id)[0]
+    def find_users(self, user_id, search_params):
+        if not search_params:
+                # Получаем значение параметра age_from из базы данных
+                age_from = select_param_user(user_id)[3]
+                if not age_from or len(age_from) < 5:
+                    age_from = self.age_from(user_id)
+                if "." in str(age_from) and len(str(age_from).split(".")) == 3:
+                    age_from = self.calculate_age(age_from)
 
-        # Меняем значение sex на противоположное
-        if int(sex) == 1:
-            search_sex = 2
-        else:
-            search_sex = 1
+                # Получаем значение параметра city из базы данных
+                city = select_param_user(user_id)[2]
+                if not city:
+                    city = self.find_city(user_id)
 
-        # Получаем значение параметра age_from из базы данных
-        age_from = select_param_user(user_id)[3]
+                search_params.update({
+                    'v': '5.131',
+                    'sex': 2 if int(select_param_user(user_id)[0]) == 1 else 1,
+                    'age_from': age_from,
+                    'city': city,
+                    'fields': 'is_closed, id, first_name, last_name',
+                    'status': '1' or '6',
+                    'has_photo': '1',
+                    'count': 999
+                })
 
-        if not age_from or len(age_from) < 5:
-            age_from = self.age_from(user_id)
-        if "." in str(age_from) and len(str(age_from).split(".")) == 3:
-            age_from = self.calculate_age(age_from)
+        try:
+            # Выполняем запрос к VK API для поиска пользователей
+            response = self.user.method('users.search', search_params)['items']
 
-        # Получаем значение параметра city из базы данных
-        city = select_param_user(user_id)[2]
-        if not city:
-            city = self.find_city(user_id)
+            # Обрабатываем результаты поиска
+            users_list = []
+            if not response:
+                self.write_msg(user_id, 'Попробуйте еще раз.')
+            else:
+                for data in response:
+                    if data["is_closed"] == False:
+                        # проверяем, был ли этот пользователь уже просмотрен
+                        viewed_user = select_viewed_user(data['id'], user_id)
+                        if viewed_user is None:
+                            # формируем список пользователей
+                            user = [data["first_name"].replace("'", ''), data["last_name"].replace("'", ''),
+                                    "vk.com/id" + str(data["id"]), str(data["id"])]
+                            users_list.append(user)
+                        else:
+                            # пользователь уже просмотрен, пропускаем его
+                            pass
 
-        # Выполняем запрос к VK API для поиска пользователей
-        response = self.user.method('users.search', {'v': '5.131',
-                                                     'sex': search_sex,
-                                                     'age_from': age_from,
-                                                     'city': city,
-                                                     'fields': 'is_closed, id, first_name, last_name',
-                                                     'status': '1' or '6',
-                                                     'has_photo': '1',
-                                                     'count': 999})['items']
+            # выводим информацию по каждой записи
+            for user in users_list:
+                yield user
 
-        # Обрабатываем результаты поиска
-        for data in response:
-            if data["is_closed"] == False:
-                first_name = data["first_name"].replace("'", '')
-                last_name = data["last_name"].replace("'", '')
-                find_vk_id = str(data["id"])
-                find_vk_link = "vk.com/id" + str(data["id"])
-                insert_users_find(first_name, user_id, last_name, find_vk_id, find_vk_link)
-        return self.write_msg(user_id, 'Поиск завершён')
-
-    def find_users_param(self, user_id):
-        sex = select_param_user(user_id)[0]
-        if int(sex) == 1:
-            search_sex = 2
-        else:
-            search_sex = 1
-        response = self.user.method('users.search', {'v': '5.131',
-                                                     'sex': search_sex,
-                                                     'age_from': self.age_from(user_id),
-                                                     'age_to': self.age_to(user_id),
-                                                     'city': self.find_city(user_id),
-                                                     'fields': 'is_closed, id, first_name, last_name',
-                                                     'status': '1' or '6',
-                                                     'has_photo': '1',
-                                                     'count': 999})['items']
-
-        for data in response:
-            if data["is_closed"] == False:
-                first_name = data["first_name"].replace("'", '')
-                last_name = data["last_name"].replace("'", '')
-                find_vk_id = str(data["id"])
-                find_vk_link = "vk.com/id" + str(data["id"])
-                insert_users_find(first_name, user_id, last_name, find_vk_id, find_vk_link)
-        return self.write_msg(user_id, 'Поиск завершён')
+        except Exception as e:
+            # обрабатываем ошибку
+            self.write_msg(user_id, f'Произошла ошибка: {e}. Попробуйте еще раз позже.')
 
     def find_photo(self, gen):
         person_id = gen[3]
@@ -159,12 +144,16 @@ class Bot:
         list_photo = [f'photo{photo["owner_id"]}_{photo["id"]}' for photo in album_photo]
         return f'{",".join(list_photo)}'
 
+
     def next_user(self, user_id, gen, message):
         self.write_msg(user_id, f'{" ".join((gen)[:3])}')
         insert_viewed_user(user_id, gen[3], gen[2])
         self.vk.method('messages.send', {'user_id': user_id,
-                                         'access_token': user_token,
-                                         'message': message,
-                                         'attachment': self.find_photo(gen),
-                                         'random_id': 0})
+                                             'access_token': user_token,
+                                             'message': message,
+                                             'attachment': self.find_photo(gen),
+                                             'random_id': 0})
         return
+
+
+
--- start_vkinder_bot.py
+++ start_vkinder_bot.py
@@ -3,31 +3,62 @@ import threading
 
 gen = {}
 
+
 def handle_user_event(event):
     if event.type == VkEventType.MESSAGE_NEW and event.to_me:
         if event.text.lower() == 'поиск':
-            Bot().delete_users_find(event.user_id)
             Bot().insert_user(event.user_id)
-            Bot().find_users(event.user_id)
-            Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
-            gen[event.user_id] = select_find_users(event.user_id)
-            Bot().next_user(event.user_id, next(gen[event.user_id]), '')
-            Bot().write_msg(event.user_id, f'Жми "Ещё"')
+            gen[event.user_id] = Bot().find_users(event.user_id, search_params={})
+            try:
+                next_user = next(gen[event.user_id])
+            except StopIteration:
+                Bot().write_msg(event.user_id, 'К сожалению, больше нет подходящих пользователей')
+            except Exception as e:
+                Bot().write_msg(event.user_id, f'Произошла ошибка: {str(e)}')
+            else:
+                Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
+                Bot().next_user(event.user_id, next_user, '')
+                Bot().write_msg(event.user_id, f'Жми "Ещё"')
         elif event.text.lower() == 'поиск по параметрам':
-            Bot().delete_users_find(event.user_id)
             Bot().insert_user(event.user_id)
-            Bot().find_users_param(event.user_id)
-            Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
-            gen[event.user_id] = select_find_users(event.user_id)
-            Bot().next_user(event.user_id, next(gen[event.user_id]), '')
-            Bot().write_msg(event.user_id, f'Жми "Ещё"')
+            params = {
+                'v': '5.131',
+                'sex': 2 if int(select_param_user(event.user_id)[0]) == 1 else 1,
+                'age_from': Bot().age_from(event.user_id),
+                'age_to': Bot().age_to(event.user_id),
+                'city': Bot().find_city(event.user_id),
+                'fields': 'is_closed, id, first_name, last_name',
+                'status': '1' or '6',
+                'has_photo': '1',
+                'count': 999
+            }
+            gen[event.user_id] = Bot().find_users(event.user_id, search_params=params)
+            try:
+                next_user = next(gen[event.user_id])
+            except StopIteration:
+                Bot().write_msg(event.user_id, 'К сожалению, больше нет подходящих пользователей')
+            except Exception as e:
+                Bot().write_msg(event.user_id, f'Произошла ошибка: {str(e)}')
+            else:
+                Bot().write_msg(event.user_id, f'Привет, {select_param_user(event.user_id)[1]}! Нашёл для тебя пару)')
+                Bot().next_user(event.user_id, next_user, '')
+                Bot().write_msg(event.user_id, f'Жми "Ещё"')
         elif event.text.lower() == 'ещё' and event.user_id in gen:
-            next_user = next(gen[event.user_id])
-            Bot().next_user(event.user_id, next_user, '')
+            try:
+                next_user = next(gen[event.user_id])
+            except StopIteration:
+                Bot().write_msg(event.user_id, 'К сожалению, больше нет подходящих пользователей')
+            except Exception as e:
+                Bot().write_msg(event.user_id, f'Произошла ошибка: {str(e)}')
+            else:
+                Bot().next_user(event.user_id, next_user, '')
+                Bot().write_msg(event.user_id, f'Жми "Ещё"')
+
 
 def listen_for_events():
     for event in Bot().longpoll.listen():
         threading.Thread(target=handle_user_event, args=(event,)).start()
+
 
 if __name__ == '__main__':
     print('Бот готов')
